7ae0a536e8e45849fd8daaa9b177a862f3d7f495
Reem Ben David <reemile1996@gmail.com>
2020-12-07 01:09:30 +0200

    css ex with Assi Oren, First commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function onInit() {
+    document.querySelectorAll('')
+}
+
+function toggleMenu() {
+    document.body.classList.toggle('open-menu');
+}
+
+function toggleModal() {
+    document.body.classList.toggle('open-modal');
+}
+
+function onHoverJournal() {
+    document.querySelector('.journal').classList.add('open-dropdown');
+}
+
+function onHoverOutJournal() {
+    document.querySelector('.journal').classList.remove('open-dropdown');
+}
